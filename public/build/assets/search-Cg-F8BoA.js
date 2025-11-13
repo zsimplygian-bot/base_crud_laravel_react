93e87da0c861d2b76dc54d7b220cdfe45abc7052
Gianfranco QC <gianfranco@example.com>
2025-11-13 12:48:42 -0500

    Descripción de los cambios

---
+++ public/build/assets/search-Cg-F8BoA.js
@@ -0,0 +1,21 @@
+import{a as e}from"./button-lX8Cm3Uw.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],a=e("ChevronDown",o);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],h=e("ChevronLeft",c);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],r=e("Plus",n);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]],d=e("Search",s);export{a as C,r as P,d as S,h as a};
