c8d4512f661cddeee1617ef367bc9925caf6507f
Gianfranco QC <gianfranco@example.com>
2025-11-24 16:50:57 -0500

    Descripción de los cambios

--- public/build/assets/input-error-D5eQ5KA_.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-N0AuUhLA.js";import{c as o}from"./button-CI3LEGGv.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
